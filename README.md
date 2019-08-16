20a19d478cd866b1d9949d66f1b23a55c787dd5f
Kevin Abram <htwnkid@gmail.com>
2019-08-15 23:36:41 -0400

    Final Version of the Liri Bot Application

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# liri-node-app
+# Liri Bot
+
+Applicatio Purpose:
+
+Liri Bot is a command line driven Web Application that provides the User with the ability to perform command searches for Concerts, Movies, and Songs.
+
+The commands available in this appication are: concert-this, spotify-this-song, movie-this, and do-what-it-says. 
+
+
+
+concert-this - This command when issued, will provide the User with concert information about their favorite Bands.
+
+            node liri.js concert-this <artist/band name here>
+
+            This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
+
+            Name of the venue
+
+            Venue location
+
+            Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+
+spotify-this-song - This command will provide the user with the ability to search the  Spotify API  
+
+            node liri.js spotify-this-song '<song name here>'
+
+            This will show the following information about the song in your terminal/bash window
+
+            Artist(s)
+
+            The song's name
+
+            A preview link of the song from Spotify
+
+            The album that the song is from
+
+            If no song is provided then your program will default to "The Sign" by Ace of Base.
+
+
+movie-this - This command will search the OMDB API and return information about any movie name entered on the command line
+
+
+            node liri.js movie-this '<movie name here>'
+
+            This will output the following information to your terminal/bash window:
+
+            * Title of the movie.
+            * Year the movie came out.
+            * IMDB Rating of the movie.
+            * Rotten Tomatoes Rating of the movie.
+            * Country where the movie was produced.
+            * Language of the movie.
+            * Plot of the movie.
+            * Actors in the movie.
+
+
+do-what-it-says - This command will read the random.txt file, and retrieve a specified song title, and look it up on the Spotify API.
+
+            It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
--- liri.js
+++ liri.js
@@ -1,13 +1,15 @@
-//var envvar = require("./dotenv").config();
-var spotify = require("Node-Spotify-API");
-var axios = require("Axios");
-var moment = require("Moment");
+require("dotenv").config();
+var Spotify = require("node-spotify-api");
+var axios = require("axios");
+var moment = require("moment");
 var keys = require("./keys.js");
+var fs = require("fs");
+
 
 //Capture the Command Line Arguments
 
 var cmdLnCmd = process.argv[2];                             //Script Command  i.e movie-this, do-what-it=says, etc
-var cmdLnArg1 = process.argv[3];
+var cmdLnArg1 = process.argv[3];                            //Movie Name
 
 switch (cmdLnCmd) {
 
@@ -21,49 +23,189 @@ switch (cmdLnCmd) {
          //    console.log(response.data);
          // });
 
-         //var re = /\ /g;
-         //var str = cmdLnArg1;
-         //var newstr = str.replace(re, '+');
+         console.log("cmdLnArg1 := " + cmdLnArg1);
 
+         if (cmdLnArg1 === undefined) {
 
-         //console.log("newstr = " + newstr);
+            newstr = "mr+nobody"; 
 
+            console.log("I got inside newstr kevin");
 
+         } else {
+
+            var re = /\ /g;
+            var str = cmdLnArg1;
+            var newstr = str.replace(re, '+');
+            
+         }
+
+         console.log("newstr = " + newstr);
 
          console.log("The Command Entered was: " + cmdLnCmd);
          console.log("The Argument Entered was: " + cmdLnArg1);
 
-         //if (newstr.length === null) {
+         axios.get("http://www.omdbapi.com/?t=" + newstr + "=&y=&plot=short&apikey=trilogy").then(
 
-         //    newstr = "mr.+nobody"
-
-         //} else
-
-         //}
-
-         //############# For Testing Purpose Only Remove   ###############################################
-
-         axios.get("http://www.omdbapi.com/?t=mr/\.+nobody=&y=&plot=short&apikey=trilogy").then(
          function(response) {
-            console.log("The Title of the Movie is: " + response.data.Title);
-            console.log("Year the movie came out is: " + response.data.Year);
-            console.log("IMDB Rating of the movie is: " + response.data.imdbRating);
-            //console.log("Rotten Tomatoes Rating of the movie is: " + response.data.imdbRating);
-            console.log("Country whee the movie was produced: " + response.data.Country);
-            console.log("Language of the movie: " + response.data.Language);
-            console.log("Plot of the movie is: " + response.data.Plot);
-            console.log("Actors in the movie are: " + response.data.Actors);
-         });
+               console.log("The Title of the Movie is: " + response.data.Title);
+               console.log("Year the movie came out is: " + response.data.Year);
+               console.log("IMDB Rating of the movie is: " + response.data.imdbRating);
+               console.log("Rotten Tomatoes Rating of the movie is: " + response.data.imdbRating);
+               console.log("Country whee the movie was produced: " + response.data.Country);
+               console.log("Language of the movie: " + response.data.Language);
+               console.log("Plot of the movie is: " + response.data.Plot);
+               console.log("Actors in the movie are: " + response.data.Actors);
+            });
 
-
-
-
-
+      
    case 'concert-this':
+
+              //axios
+              //   .get("https://api.spotify.com")
+              //   .then(function(response) {
+              // If the axios was successful...
+              // Then log the body from the site!
+              //    console.log(response.data);
+              // });
+               
+            console.log("cmdLnArg1 := " + cmdLnArg1);
+               
+            if (cmdLnArg1 === undefined) {
+               
+                  newstr = "mr+nobody"; 
+               
+                  console.log("I got inside newstr kevin");
+               
+            } else {
+               
+                  //var re = /\ /g;
+                  //var str = cmdLnArg1;
+                  //var newstr = str.replace(re, '+');
+                           
+              
+               
+                  //console.log("newstr = " + newstr);
+               
+                  //console.log("The Command Entered was: " + cmdLnCmd);
+                  //console.log("The Argument Entered was: " + cmdLnArg1);
+               
+                  //axios.get("https://rest.bandsintown.com/artists/" + newstr + "?app_id=codingbootcamp").then(
+
+                  axios.get("https://rest.bandsintown.com/artists/+maroon+5"+"?app_id=codingbootcamp").then(
+
+
+                  function(response) {
+
+                        console.log("Response Data := " + response);
+                        
+                        //console.log("Name of the venue: " + response.data.name);
+                        //console.log("Venue location: " + response.data.Year);
+                        //console.log("Date of the Event: " + response.data.imdbRating);
+
+                  }
+
+            
+
+
+            )} 
+
+   
    case 'spotify-this-song':
+
+                  cmdLnCmd = process.argv[2];                             //Script Command  i.e movie-this, do-what-it=says, etc
+                  cmdLnArg1 = process.argv[3];                            //Song Name
+         
+                  if (cmdLnArg1 === undefined) {
+
+                        newstr = "the+sign"; 
+                                           
+                     } else {
+            
+                        var re = /\ /g;
+                        var str = cmdLnArg1;
+                        var newstr = str.replace(re, '+');
+                        
+                     }
+         
+         
+                  //Creating a new spotify object
+         
+                  var spotify = new Spotify(keys.spotify);
+         
+                  spotify.search({ type: 'track', query: newstr }, function(err,data) {
+         
+                  if(err) {
+         
+                       return console.log('Error occurred: ' + err);
+         
+                  }
+         
+                  console.log(data);
+         
+                  for (i in items[0].Objects) {
+         
+                      console.log(tracks.Objects[0]);
+         
+                  }
+         
+                });
+                         
+         
+                         
+         
+            
+
    case 'do-what-it-says':
 
+         fs.readFile("random.txt", "utf8", function(error, data) {
+
+         // If the code experiences any errors it will log the error to the console.
+
+         if (error) {
+
+             return console.log(error);
+
+         }
+
+         console.log("Data Read from Random.txt file: " + data);
+
+         var str = data;
+
+         var wordparts = str.split(',');
+
+         console.log("The first part is: "+ wordparts[0]);
+
+         console.log("The first part is: "+ wordparts[1]);
+
+         //Creating a new spotify object
+
+         var spotify = new Spotify(keys.spotify);
+
+         spotify.search({ type: 'track', query: wordparts[1] }, function(err,data) {
+
+         if(err) {
+
+              return console.log('Error occurred: ' + err);
+
+         }
+
+         console.log(data);
+
+         for (i in items[0].Objects) {
+
+             console.log(tracks.Objects[0]);
+
+         }
+
+       });
+                
+
+                
+
+   });
+
 }
+
 
 
 //axios
@@ -79,8 +221,8 @@ switch (cmdLnCmd) {
 
 //Capture the Command Line Arguments
 
-var cmdLnCmd = process.argv[2];                             //Script Command  i.e movie-this, do-what-it=says, etc
-var cmdLnArg1 = process.argv[3];                           //Movie Title
+//var cmdLnCmd = process.argv[2];                             //Script Command  i.e movie-this, do-what-it=says, etc
+//var cmdLnArg1 = process.argv[3];                           //Movie Title
 
 
 
